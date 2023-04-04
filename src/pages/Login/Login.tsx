e3b2f0bb1aa5c52e60637e5040c0e7bdcac09ab7
phuoc15258 <nguyendonphuochusc@gmail.com>
2023-04-05 00:34:20 +0700

    [feat/LOGIN_PAGE]: build ui Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import LoginComponent from '../../components/Layout/Login/Login';
 
 const Login = (): JSX.Element => {
-    return <></>;
+    return <LoginComponent />;
 };
 
 export default Login;
